631b070f69d24aa46bfb18c2241d3c406221d7a7
grop4all <nikitatikhonov063@gmail.com>
2021-11-01 22:23:27 +0500

    форма для отправки нужно сделать

--- crm/models.py
+++ crm/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Order(models.Model):
-    order_dt = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length=200)
-    order_phone = models.CharField(max_length=200)
+    order_dt = models.DateTimeField(auto_now=True , verbose_name='Дата')
+    order_name = models.CharField(max_length=200, verbose_name='Имя')
+    order_phone = models.CharField(max_length=200, verbose_name='Номер телефона')
 
     def __str__(self):
         return f'{self.order_name}'
--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Order
 
+
+def main_page(request):
+    object_list = Order.objects.all()
+    Title = 'main_page'
+    return render(request, './index.html', {
+        'object_list': object_list,
+        'Title': Title
+    })
 # Create your views here.
